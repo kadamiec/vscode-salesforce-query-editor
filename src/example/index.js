f84e015dc8fc0ad77768310b5f42369b34a66f04
Allan Oricil <allanoricilcos@outlook.com>
2020-07-10 17:33:54 -0300

    Add vscode telemetry

--- src/example/index.js
+++ src/example/index.js
@@ -3,24 +3,39 @@ const vscode = require('vscode');
 const fs = require('fs-extra');
 const path = require('path');
 const EGWebView = require('./e.g.webview');
+const TelemetryReporter = require('vscode-extension-telemetry');
+
 const {
   setupSchemaGlobalDirectory
 } = require('../lib/utils.js');
 
 const name = 'SOQL Editor';
 const webview = new EGWebView();
+
+// all events will be prefixed with this event name
+const extensionId = 'allanoricil.salesforce-soql-editor';
+
+// extension version will be reported as a property with each event
+const extensionVersion = '0.2.8';
+
+// the application insights key (also known as instrumentation key)
+const key = '4ea9b1db-69cd-4355-8a3e-eac076d7325c';
+
+let reporter;
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 const activate = (context) => {
+  reporter = new TelemetryReporter.default(extensionId, extensionVersion, key);
   webview.activate(context, name, 'SFDX.soqlEditor');
-  vscode.window.showInformationMessage(
-    'Salesforce SOQL Editor is Activated'
-  );
+  vscode.window.showInformationMessage('Salesforce SOQL Editor is Activated');
+  reporter.sendTelemetryEvent('activation');
 };
 
 const deactivate = () => {
   webview.deactivate();
+  reporter.dispose();
 };
 
 module.exports = {
