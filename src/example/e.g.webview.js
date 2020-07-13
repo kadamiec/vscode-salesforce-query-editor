9faeb66bc64248953fe36e059e35f030b20d28fd
Allan Oricil <allanoricilcos@outlook.com>
2020-07-12 22:22:52 -0300

    Add Metrics for time to complete a query, errors during a query and if the user clicked on the button to add in the apex

--- src/example/e.g.webview.js
+++ src/example/e.g.webview.js
@@ -1,14 +1,12 @@
 // @ts-nocheck
-const {
-    WebView
-} = require('../vscode/vscode.webview');
+const { WebView } = require('../vscode/vscode.webview');
 const vscode = require('vscode');
 const {
-    refreshSObjects,
-    getOrgDisplay,
-    getGlobalDescribe,
-    callSObjectDescribe,
-    executeSOQL,
+  refreshSObjects,
+  getOrgDisplay,
+  getGlobalDescribe,
+  callSObjectDescribe,
+  executeSOQL,
 } = require('../lib/utils.js');
 /**
  *Add business
@@ -18,114 +16,133 @@ const {
  */
 
 class EGWebView extends WebView {
-    /**
-     * Creates an instance of EGWebView.
-     * @memberof EGWebView
-     */
+  /**
+   * Creates an instance of EGWebView.
+   * @memberof EGWebView
+   */
 
-    constructor() {
-        super();
+  constructor(reporter) {
+    super();
 
+    this.defaultOrg = getOrgDisplay();
+
+    this.activeTextEditor = vscode.window.activeTextEditor;
+
+    this.reporter = reporter;
+
+    this.onDidPose = () => {
+      this.defaultOrg = getOrgDisplay();
+    };
+
+    this.didChangeViewState = () => {
+      this.activeTextEditor = vscode.window.activeTextEditor;
+    };
+
+    this.handler.addApi({
+      getAllObjectNames: () => {
+        try {
+          getGlobalDescribe(this.defaultOrg).then((response) => {
+            this.postMessage('objects', response.data.sobjects);
+          });
+        } catch (e) {
+          this.showErrorMessage("Couldn't get the SObjects");
+          this.channel.appendLine(e);
+        }
+      },
+      refreshSObjects: () => {
         this.defaultOrg = getOrgDisplay();
-
-        this.activeTextEditor = vscode.window.activeTextEditor;
-
-        this.onDidPose = () => {
-            this.defaultOrg = getOrgDisplay();
-        };
-
-        this.didChangeViewState = ()=>{
-            this.activeTextEditor = vscode.window.activeTextEditor;
-        }; 
-
-        this.handler.addApi({
-            getAllObjectNames: () => {
-                try {
-                    getGlobalDescribe(this.defaultOrg).then((response) => {
-                        this.postMessage('objects', response.data.sobjects);
-                    });
-                } catch (e) {
-                    this.showErrorMessage("Couldn't get the SObjects");
-                    this.channel.appendLine(e);
+        refreshSObjects(this.defaultOrg)
+          .then((response) => {
+            this.postMessage('objects', response.data.sobjects);
+          })
+          .catch((e) => {
+            vscode.window
+              .showErrorMessage("Couldn't Refresh SObjects", 'Show Output')
+              .then((selection) => {
+                if (selection === 'Show Output') {
+                  this.channel.show();
                 }
-            },
-            refreshSObjects: () => {
-                this.defaultOrg = getOrgDisplay();
-                refreshSObjects(this.defaultOrg)
-                    .then((response) => {
-                        this.postMessage('objects', response.data.sobjects);
-                    })
-                    .catch((e) => {
-                        vscode.window
-                            .showErrorMessage("Couldn't Refresh SObjects", 'Show Output')
-                            .then((selection) => {
-                                if (selection === 'Show Output') {
-                                    this.channel.show();
-                                }
-                            });
-                        this.postMessage('objects');
-                        this.channel.appendLine(e);
-                    });
-            },
-            getSObjectDescribe: (sObject) => {
-                callSObjectDescribe(this.defaultOrg, sObject).then((response) => {
-                    this.postMessage('sobjectDescription', response.data);
-                });
-            },
-            executeSOQL: (soql) => {
-                executeSOQL(soql, this.defaultOrg).then((response) => {
-                    this.postMessage('soqlResult', response.data.records);
-                }).catch((reason) =>{
-                    if(reason.response.status === 400)
-                        this.postMessage('soqlResult', reason.response.data[0]);
-                    else
-                        this.postMessage('soqlResult', []);
-                });
-            },
-            addToApex: (soql) =>{
-                // current editor
-                const editor = this.activeTextEditor;
-
-                // check if there is no selection
-                if (editor.selection.isEmpty) {
-                    // the Position object gives you the line and character where the cursor is
-                    const position = editor.selection.active;
-                    editor.edit((editBuilder)=>{
-                        editBuilder.insert(position, `[${soql}]`);
-                    });
-                }
-            }
+              });
+            this.postMessage('objects');
+            this.channel.appendLine(e);
+          });
+      },
+      getSObjectDescribe: (sObject) => {
+        callSObjectDescribe(this.defaultOrg, sObject).then((response) => {
+          this.postMessage('sobjectDescription', response.data);
         });
-    }
+      },
+      executeSOQL: (soql) => {
+        const start = new Date();
+        executeSOQL(soql, this.defaultOrg)
+          .then((response) => {
+            this.postMessage('soqlResult', response.data.records);
 
-    /**
-     * Activate
-     * @param {import('vscode').ExtensionContext} context vscode extension context
-     * @param {string} name webview name
-     * @param {string} cmdName cmd name
-     * @param {string} [htmlPath=path.join(context.extensionPath, 'web', 'dist', 'index.html')] html path
-     * @returns {this}
-     * @memberof WebView
-     */
-    activate(context, name, cmdName, htmlPath = undefined) {
-        // custom code if need
-        return super.activate(context, name, cmdName, htmlPath);
-    }
+            const elapsedTimeToQuery = new Date() - start;
+            this.reporter.sendTelemetryEvent(
+              'executed-soql',
+              {
+                soql: soql,
+              },
+              { elapsedTimeToQuery: elapsedTimeToQuery }
+            );
+          })
+          .catch((reason) => {
+            if (reason.response.status === 400)
+              this.postMessage('soqlResult', reason.response.data[0]);
+            else this.postMessage('soqlResult', []);
 
-    showErrorMessage(message) {
-        vscode.window.showErrorMessage(message, 'Show Output').then((selection) => {
-            if (selection === 'Show Output') {
-                this.channel.show();
-            }
-        });
-    }
+            this.reporter.sendTelemetryException(reason, { soql: soql });
+          });
+      },
+      addToApex: (soql) => {
+        // current editor
+        const editor = this.activeTextEditor;
 
-    postMessage(message, data) {
-        this.panel.webview.postMessage({
-            cmd: message,
-            data: data,
-        });
-    }
+        // check if there is no selection
+        if (editor.selection.isEmpty) {
+          // the Position object gives you the line and character where the cursor is
+          const position = editor.selection.active;
+          editor.edit((editBuilder) => {
+            editBuilder.insert(position, `[${soql}]`);
+          });
+
+          this.reporter.sendTelemetryEvent('add-to-apex', {
+            soql: soql,
+          });
+        }
+      },
+    });
+  }
+
+  /**
+   * Activate
+   * @param {import('vscode').ExtensionContext} context vscode extension context
+   * @param {string} name webview name
+   * @param {string} cmdName cmd name
+   * @param {string} [htmlPath=path.join(context.extensionPath, 'web', 'dist', 'index.html')] html path
+   * @returns {this}
+   * @memberof WebView
+   */
+  activate(context, name, cmdName, htmlPath = undefined) {
+    // custom code if need
+    return super.activate(context, name, cmdName, htmlPath);
+  }
+
+  showErrorMessage(message) {
+    vscode.window.showErrorMessage(message, 'Show Output').then((selection) => {
+      if (selection === 'Show Output') {
+        this.channel.show();
+      }
+    });
+  }
+
+  postMessage(message, data) {
+    this.panel.webview.postMessage({
+      cmd: message,
+      data: data,
+    });
+  }
 }
 
 module.exports = EGWebView;
--- src/example/index.js
+++ src/example/index.js
@@ -4,7 +4,6 @@ const EGWebView = require('./e.g.webview');
 const TelemetryReporter = require('vscode-extension-telemetry');
 
 const name = 'SOQL Editor';
-const webview = new EGWebView();
 
 // all events will be prefixed with this event name
 const extensionId = 'allanoricil.salesforce-soql-editor';
@@ -15,16 +14,22 @@ const extensionVersion = '0.2.8';
 // the application insights key (also known as instrumentation key)
 const key = '4ea9b1db-69cd-4355-8a3e-eac076d7325c';
 
-let reporter;
+const reporter = new TelemetryReporter.default(
+  extensionId,
+  extensionVersion,
+  key
+);
+
+const webview = new EGWebView(reporter);
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 const activate = (context) => {
-  reporter = new TelemetryReporter.default(extensionId, extensionVersion, key);
   webview.activate(context, name, 'SFDX.soqlEditor');
   vscode.window.showInformationMessage('Salesforce SOQL Editor is Activated');
   reporter.sendTelemetryEvent('activation');
+  context.subscriptions.push(reporter);
 };
 
 const deactivate = () => {
