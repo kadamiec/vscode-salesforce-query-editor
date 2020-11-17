bc7bb05275ac1ffba0bdd565199d32a9eb9247b1
Allan Oricil <allanoricilcos@outlook.com>
2020-11-17 00:09:44 +0000

    Update buy me a coffee donation button. Add Paypal QR Code

--- web/src/components/editor.vue
+++ web/src/components/editor.vue
@@ -2,12 +2,16 @@
   <div class="container vh-100 vw-100 py-2">
     <div v-if="loading" class="d-flex justify-content-center h-100">
       <div class="m-auto">
+        <div class="d-flex flex-column">
+          <h2 class="mx-auto mb-3">Buy me a coffee</h2>
           <div class="d-flex flex-column">
-            <h2>Buy me a Coffee</h2>
-            <a target="_blank" class="mx-auto" href="https://www.buymeacoffee.com/allanoricil" >
-              <img src="../../static/images/buyMeACoffeIcon.svg" width="200px;" alt="Kiwi standing on oval">
-            </a>
-            <stretch background="var(--vscode-button-background)" class="mt-4 mx-auto"></stretch>
+            <a href="https://www.buymeacoffee.com/allanoricil" target="_blank"><img src="../../static/images/buymeacoffee.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
+            <div class="donation-card d-flex flex-column py-4 mx-auto mt-3">
+              <img src="../../static/images/paypallogo.png" width="128px;" class="mx-auto mb-2"/>
+              <img src="../../static/images/QR Code.png" width="128px;" class="mx-auto"/>
+            </div>
+          </div>
+          <stretch background="var(--vscode-button-background)" class="mt-4 mx-auto"></stretch>
         </div>
       </div>
     </div>
@@ -937,5 +941,22 @@ export default {
 
 .field-has-next:hover{
   color: var(--vscode-button-background) !important;
+}
+
+.donation-card{
+  border-radius: 1rem; 
+  background-color: white; 
+  width: 200px;
+}
+
+.generate-btn-preview{
+    height: 60px;
+    border-radius: 12px;
+    background-color: #FFDD00;
+    color: #000;
+    font-size: 32px;
+    border: none;
+    padding: 0px 24px;
+    font-family: 'Cookie', cursive;
 }
 </style>
