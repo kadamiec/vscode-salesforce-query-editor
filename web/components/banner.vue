b80380fac077b4f389774927c9c52a6ebc18f498
Allan Oricil <allanoricilcos@outlook.com>
2021-04-07 13:39:02 +0100

    Add five stars button in the banner component

---
+++ web/components/banner.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="d-flex">
+    <img
+      v-b-tooltip.hover
+      src="~/assets/images/five_stars.png"
+      title="Leave a nice review"
+      @click="onClickFiveStars"
+    />
+    <button class="golden-btn ml-auto mb-2" @click="onClickBuyProButton">
+      BUY PRO
+    </button>
+  </div>
+</template>
+
+<script>
+import showToastMessage from '~/mixins/show-toast-message'
+
+export default {
+  mixins: [showToastMessage],
+  methods: {
+    onClickBuyProButton() {
+      this.showToastMessage('Opening the Product Description Page.')
+      this.$axios
+        .post(
+          `${process.env.SALESFORCE_SERVER}/vscode/page/open`,
+          {
+            page: 'https://salesforcequeryeditor.com/#/product-description',
+          },
+          {
+            headers: {
+              'Content-Type': 'application/vnd.api+json',
+              Accept: 'application/vnd.api+json',
+            },
+          }
+        )
+        .catch(() => {
+          this.showToastMessage('Could not open the Product Description Page')
+        })
+    },
+    onClickFiveStars() {
+      this.showToastMessage('Opening VS Code Marketplace page.')
+      this.$axios
+        .post(
+          `${process.env.SALESFORCE_SERVER}/vscode/page/open`,
+          {
+            page:
+              'https://marketplace.visualstudio.com/items?itemName=allanoricil.salesforce-soql-editor&ssr=false#review-details',
+          },
+          {
+            headers: {
+              'Content-Type': 'application/vnd.api+json',
+              Accept: 'application/vnd.api+json',
+            },
+          }
+        )
+        .catch(() => {
+          this.showToastMessage('Could not open the VS Code Marketplace page.')
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+img {
+  height: 30px;
+  cursor: pointer;
+}
+
+.golden-btn + .golden-btn {
+  margin-top: 1em;
+}
+
+.golden-btn {
+  width: 100px;
+  display: inline-block;
+  outline: none;
+  font-family: inherit;
+  font-size: 1em;
+  box-sizing: border-box;
+  font-weight: bold;
+  border: none;
+  border-radius: 0.3em;
+  height: 2.75em;
+  line-height: 2.5em;
+  text-transform: uppercase;
+  padding: 0 1em;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(110, 80, 20, 0.4),
+    inset 0 -2px 5px 1px rgba(139, 66, 8, 1),
+    inset 0 -1px 1px 3px rgba(250, 227, 133, 1);
+  background-image: linear-gradient(
+    160deg,
+    #a54e07,
+    #b47e11,
+    #fef1a2,
+    #bc881b,
+    #a54e07
+  );
+  border: 1px solid #a55d07;
+  color: rgb(120, 50, 5);
+  text-shadow: 0 2px 2px rgba(250, 227, 133, 1);
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  background-size: 100% 100%;
+  background-position: center;
+}
+.golden-btn:focus,
+.golden-btn:hover {
+  background-size: 150% 150%;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23),
+    inset 0 -2px 5px 1px #b17d10, inset 0 -1px 1px 3px rgba(250, 227, 133, 1);
+  border: 1px solid rgba(165, 93, 7, 0.6);
+  color: rgba(120, 50, 5, 0.8);
+}
+.golden-btn:active {
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(110, 80, 20, 0.4),
+    inset 0 -2px 5px 1px #b17d10, inset 0 -1px 1px 3px rgba(250, 227, 133, 1);
+}
+</style>
--- web/components/editor.vue
+++ web/components/editor.vue
@@ -3,13 +3,7 @@
     <loading v-if="isLoadingData"></loading>
     <div v-show="!isLoadingData" :ref="name" class="editor px-2 pt-2">
       <div class="d-flex flex-column">
-        <button
-          v-if="!isLicenseValid()"
-          class="golden-btn ml-auto mb-2"
-          @click="onClickBuyProButton"
-        >
-          BUY PRO
-        </button>
+        <banner v-if="!isLicenseValid()"/>
         <!-- ENVIRONMENTS -->
         <div
           v-if="environments && environments.length"
@@ -374,6 +368,7 @@ import QueryTextEditor from '@/components/query-text-editor.vue'
 import QueryResults from '@/components/query-results.vue'
 import SoqlPlan from '@/components/soql-plan.vue'
 import Loading from '@/components/loading.vue'
+import Banner from '@/components/banner.vue'
 
 import { decode } from 'html-entities'
 import sqlFormatter from '@allanoricil/sql-formatter'
@@ -392,6 +387,7 @@ export default {
     QueryResults,
     SoqlPlan,
     Loading,
+    Banner
   },
   mixins: [showToastMessage],
   props: {
@@ -1204,25 +1200,6 @@ export default {
     onClickRefreshEnvironmentsButton() {
       this.$emit('reloadEnvironments')
     },
-    onClickBuyProButton() {
-      this.showToastMessage('Opening the Product Description Page.')
-      this.$axios
-        .post(
-          `${process.env.SALESFORCE_SERVER}/vscode/page/open`,
-          {
-            page: 'https://salesforcequeryeditor.com/#/product-description',
-          },
-          {
-            headers: {
-              'Content-Type': 'application/vnd.api+json',
-              Accept: 'application/vnd.api+json',
-            },
-          }
-        )
-        .catch(() => {
-          this.showToastMessage('Could not open the Product Description Page')
-        })
-    },
     onCopySoqlText() {
       this.showToastMessage('Query copied with success.')
     },
@@ -1281,56 +1258,6 @@ function removeKeys(obj, keys) {
 
 /deep/ .nav-tabs {
   border-bottom: none !important;
-}
-
-.golden-btn + .golden-btn {
-  margin-top: 1em;
-}
-
-.golden-btn {
-  width: 100px;
-  display: inline-block;
-  outline: none;
-  font-family: inherit;
-  font-size: 1em;
-  box-sizing: border-box;
-  font-weight: bold;
-  border: none;
-  border-radius: 0.3em;
-  height: 2.75em;
-  line-height: 2.5em;
-  text-transform: uppercase;
-  padding: 0 1em;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(110, 80, 20, 0.4),
-    inset 0 -2px 5px 1px rgba(139, 66, 8, 1),
-    inset 0 -1px 1px 3px rgba(250, 227, 133, 1);
-  background-image: linear-gradient(
-    160deg,
-    #a54e07,
-    #b47e11,
-    #fef1a2,
-    #bc881b,
-    #a54e07
-  );
-  border: 1px solid #a55d07;
-  color: rgb(120, 50, 5);
-  text-shadow: 0 2px 2px rgba(250, 227, 133, 1);
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  background-size: 100% 100%;
-  background-position: center;
-}
-.golden-btn:focus,
-.golden-btn:hover {
-  background-size: 150% 150%;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23),
-    inset 0 -2px 5px 1px #b17d10, inset 0 -1px 1px 3px rgba(250, 227, 133, 1);
-  border: 1px solid rgba(165, 93, 7, 0.6);
-  color: rgba(120, 50, 5, 0.8);
-}
-.golden-btn:active {
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(110, 80, 20, 0.4),
-    inset 0 -2px 5px 1px #b17d10, inset 0 -1px 1px 3px rgba(250, 227, 133, 1);
 }
 
 .environment /deep/ .multiselect__tags {
