429fabb9fdd9366dc0ae95f8449e7a20071b7a28
Allan Oricil <allanoricilcos@outlook.com>
2021-04-11 15:24:43 +0100

    Fix email verification

--- web/pages/email-verification.vue
+++ web/pages/email-verification.vue
@@ -7,8 +7,17 @@ export default {
   created() {
     this.$axios
       .get(
-        `${process.env.WEBHOOKS_SERVER}/verification?code=${this.$route.query.code}`
+        `${process.env.WEBHOOKS_SERVER}/verification?code=${this.$route.query.code}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        }
       )
+      .then(() => {
+        this.$router.push({ name: 'success-verification' })
+      })
       .catch(() => {
         this.$router.push({ name: 'bad-verification' })
       })
