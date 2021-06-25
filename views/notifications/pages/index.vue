79dd0c66c65e9398a1fd5b01b56e1891b8b620d0
Allan Oricil <allanoricilcos@outlook.com>
2021-06-24 22:18:18 -0300

    remove github workflow

--- views/notifications/pages/index.vue
+++ views/notifications/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column vh-100 vw-100 p-3">
     <p class="title mb-1">Release Notes</p>
-    <p class="subtitle">v1.7.4</p>
+    <p class="subtitle">v1.8.0</p>
     <ul>
       <li>
         Fixed the bug that was not allowing the text area to change the color theme.
